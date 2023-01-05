4ce4e4f6e632f03001d8034253681c9dc2040248
DK0280705 <deka.ganteng028@gmail.com>
2023-01-06 00:06:47 +0700

    It moves now

--- src/mob/base_mob.gd
+++ src/mob/base_mob.gd
@@ -6,8 +6,10 @@ const GRAVITY: float = 20.0
 var health: int
 
 func _ready() -> void:
+	print("Base_mob instance created")
 	pass
 
+# Function specific variables
 var motion: Vector2 = Vector2.ZERO # Basic motion
 var snap:   Vector2 = Vector2.ZERO # Snap to moving platform
 func _physics_process(delta) -> void:
@@ -15,7 +17,14 @@ func _physics_process(delta) -> void:
 	motion.y += GRAVITY
 	
 	# Apply motion to body
-	move_and_slide_with_snap(motion, snap, Vector2.UP, true, 4, 0.785398, false)
+	motion = move_and_slide_with_snap(motion, snap, Vector2.UP, true, 4, 0.785398, false)
+
+# Function specific variables
+var _is_jumped: bool = false
+func jump(power: float) -> void:
+	if is_on_floor(): 
+		motion.y -= power
+		_is_jumped = true
 
 func die() -> void:
 	pass
--- src/mob/player.gd
+++ src/mob/player.gd
@@ -1,7 +1,33 @@
 extends Base_mob
 
+const MOVE_SPEED: float = 20.0
+const MAX_SPEED:  float = 200.0
+const JUMP_POWER: float = 400.0
+
 var xp: int
 var level: int
 
 func _ready():
 	pass # Replace with function body.
+
+var input_power: float
+func _physics_process(delta):
+	input_power = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	# --- Modify motion x value ---
+	
+	# If there's input power, give motion a power
+	if (input_power != 0):
+		motion.x += input_power * MOVE_SPEED
+		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+	# If there's not input power and the player is still moving
+	# decelerate the player body
+	elif (abs(motion.x) > 1):
+		motion.x = lerp(motion.x, 0, 0.2)
+	else:
+		motion.x = 0
+	
+	# --- Modify motion y value ---
+	
+	if (Input.is_action_just_pressed("jump")):
+		jump(JUMP_POWER)
