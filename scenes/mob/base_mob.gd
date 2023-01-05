fde1bbdd405fafe846bca8aa689b33cf9d3a0bd9
DK0280705 <deka.ganteng028@gmail.com>
2023-01-05 22:58:20 +0700

    Added player and base mob class

---
+++ scenes/mob/base_mob.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+class_name Base_mob
+
+var health: int
+
+func _ready() -> void:
+	pass
+
+func die() -> void:
+	pass
+
+func spawn() -> void:
+	pass
---
+++ scenes/mob/player.gd
@@ -0,0 +1,7 @@
+extends Base_mob
+
+var xp: int
+var level: int
+
+func _ready():
+	pass # Replace with function body.
