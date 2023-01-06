2a177baa7c076b11242ca17e8f268afb94c8de61
DK0280705 <deka.ganteng028@gmail.com>
2023-01-06 20:08:06 +0700

    functional shake

--- src/world/base_map.gd
+++ src/world/base_map.gd
@@ -17,16 +17,28 @@ func _input(event: InputEvent) -> void:
 	elif (event.is_action_released("ui_cancel")):
 		_pressed_cancel = false
 
+# None to shake or shake to none
+var _shake_raise: bool = false
 # More power more wiggle wiggle wiggle
 var _shake_power: float = 0.0
+var _quake_power: float = 0.0
 # Less decay last longer
 var _shake_decay: float = 0.0
 func _process(delta: float) -> void:
 	if (Input.is_action_just_pressed("debug")):
-		shake(60.0, 1.0)
+		shake(10.0, 20.0) # Default hit shake
 	
-	if (_shake_power > 0.0):
-		_shake_power = lerp(_shake_power, 0, _shake_decay * delta)
+	if (_shake_power >= 1.0):
+		if (_shake_raise):
+			_shake_power = lerp(_shake_power, _quake_power, _shake_decay * delta)
+		else:
+			_shake_power = lerp(_shake_power, 0, _shake_decay * delta)
+		
+		if (_shake_power > (_quake_power - 1)):
+			_shake_raise = false
+			_quake_power = 0
+		
+		# Change camera offset to make shake effect
 		camera.offset = Vector2(
 			random.randf_range(-_shake_power, _shake_power),
 			random.randf_range(-_shake_power, _shake_power)
@@ -36,5 +48,12 @@ func _process(delta: float) -> void:
 		_shake_power  = 0
 
 func shake(power: float, decay: float) -> void:
+	_shake_raise = false
 	_shake_power = power
+	_shake_decay = decay
+
+func quake(power: float, decay: float) -> void:
+	_shake_raise = true
+	_shake_power = 1.0
+	_quake_power = power
 	_shake_decay = decay
