deb57f2bd82a668634f8baa016a9765598189e33
DK0280705 <deka.ganteng028@gmail.com>
2023-01-06 19:47:35 +0700

    Test shake, Added Ctrl + X (Debug test key)

--- src/world/base_map.gd
+++ src/world/base_map.gd
@@ -1,9 +1,12 @@
 extends Node2D
 class_name Base_map
 
+onready var camera: Base_camera = get_node("camera")
+onready var player: Player      = get_node("player")
+
+onready var random: RandomNumberGenerator = RandomNumberGenerator.new()
+
 func _ready() -> void:
-	var camera: Base_camera = get_node("camera")
-	var player: Player      = get_node("player")
 	camera.set_focused(player)
 
 var _pressed_cancel: bool = false
@@ -13,3 +16,25 @@ func _input(event: InputEvent) -> void:
 		# Execute some pause menu
 	elif (event.is_action_released("ui_cancel")):
 		_pressed_cancel = false
+
+# More power more wiggle wiggle wiggle
+var _shake_power: float = 0.0
+# Less decay last longer
+var _shake_decay: float = 0.0
+func _process(delta: float) -> void:
+	if (Input.is_action_just_pressed("debug")):
+		shake(60.0, 1.0)
+	
+	if (_shake_power > 0.0):
+		_shake_power = lerp(_shake_power, 0, _shake_decay * delta)
+		camera.offset = Vector2(
+			random.randf_range(-_shake_power, _shake_power),
+			random.randf_range(-_shake_power, _shake_power)
+		)
+	else:
+		camera.offset = Vector2.ZERO
+		_shake_power  = 0
+
+func shake(power: float, decay: float) -> void:
+	_shake_power = power
+	_shake_decay = decay
