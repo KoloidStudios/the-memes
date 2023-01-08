0d803d71d9845691e2fe2c636d886843c2aaf160
DK0280705 <deka.ganteng028@gmail.com>
2023-01-08 23:39:27 +0700

    Added win and lose animation

--- src/world/arm_wrestling.gd
+++ src/world/arm_wrestling.gd
@@ -13,20 +13,29 @@ func _ready():
 func _process(delta):
 	if (!_started): return
 	if (progress.value <= 0):
+		shake(10, 20)
 		_started = false
+		animation.play("lose")
 		return
 	if (progress.value >= 100):
+		shake(10, 20)
 		_win = true
 		_started = false
+		animation.play("win")
 		return
 	
 	progress.value -= 30 * delta
 	
 	if (Input.is_action_just_pressed("ui_accept")):
+		shake(2, 20)
 		progress.value += 5
 	
 func start():
 	_started = true
 
 func restart():
-	pass
+	print_debug("restarted")
+
+func _on_animation_animation_finished(anim_name):
+	if (anim_name == "lose"):
+		enter_confirm("Retry?", funcref(self, "restart"))
